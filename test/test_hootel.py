5749d16c6f9bc25a4b03c8cb906d47794e8e4564
albrechtanna <142808831+albrechtanna@users.noreply.github.com>
2024-01-13 20:45:25 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -2,8 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import allure
 import pytest
+import random
+
 
 
 class TestHootel(object):
